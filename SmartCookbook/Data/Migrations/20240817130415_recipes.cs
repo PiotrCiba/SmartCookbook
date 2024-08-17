48e65a2bfb8dbffae61c304b5863ffacb07b23b6
PiotrCiba <piotr.m.ciba@gmail.com>
2024-08-17 15:04:38 +0200

    recipes

---
+++ SmartCookbook/Data/Migrations/20240817130415_recipes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartCookbook.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class recipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
